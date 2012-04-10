88608d1a4722a422da55f30ce5754884149bf2eb
Stephan Gabler <stephan.gabler@gmail.com>
2012-04-10 11:08:04 +0200

    remove trailing and leading whitespace in the pipeline file

--- NeuralImageProcessing/pipeline.py
+++ NeuralImageProcessing/pipeline.py
@@ -3,16 +3,16 @@ import numpy as np
 import json
 
 class Event(object):
-    
+
     def __init__(self, key, value):
         self.key = key
         self.value = value
-        
+
 class FullBufferException(Exception):
     pass
 
 class Block(object):
-    
+
     def __init__(self, name='block'):
         self.name = name
         self.event_listener = {}
@@ -20,32 +20,32 @@ class Block(object):
         self.event_sender = {}
         self.input = {}
         self.signals = []
-        
+
     def add_listener(self, listener, asinput='image_series'):
         self.event_listener[id(listener)] = listener
         self.listener_inputtype[id(listener)] = asinput
         listener.event_sender[id(self)] = self
-        
+
     def add_sender(self, sender, asinput='image_series'):
         self.event_sender[id(sender)] = sender
         if asinput in self.input:
             raise Exception('slot already used')
-        self.input[asinput] = '' 
+        self.input[asinput] = ''
         sender.event_listener[id(self)] = self
         sender.listener_inputtype[id(self)] = asinput
-    
+
     def release_from_listener(self):
         for listener in self.event_listener.itervalues():
             listener.event_sender.pop(id(self))
         self.event_listener = {}
         self.listener_inputtype = {}
-    
+
     def release_from_sender(self):
         for sender in self.event_sender.itervalues():
             sender.event_listener.pop(id(self))
             sender.listener_inputtype.pop(id(self))
         self.event_sender = {}
- 
+
     def sent_event(self, element):
         for key, listener in self.event_listener.iteritems():
             try:
@@ -56,7 +56,7 @@ class Block(object):
     def sent_signal(self, element):
         for listener in self.event_listener.values():
             listener.receive_event(Event('signal', element))
-                           
+
     def receive_event(self, event):
         if event.key == 'signal':
             self.process_signal(event)
@@ -70,13 +70,13 @@ class Block(object):
         else:
             print self.name, ": !!!FullBuffer!!!"
             raise FullBufferException()
-        
+
     def process_signal(self, signal_event):
         self.signals.append(signal_event)
         if len(self.signals) == len(self.event_sender):
             self.signals = []
             self.sent_signal(signal_event.value)
-        
+
     def set_receiving(self):
         """reset input buffers after processing"""
         for key in self.input:
@@ -113,62 +113,62 @@ class TimeSeries(object):
     @property
     def timepoints(self):
         return self.timecourses.shape[0] / len(self.label_sample)
-    
+
     @property
     def num_trials(self):
         return len(self.label_sample)
-    
+
     @property
     def num_objects(self):
         if type(self.shape) == type([]):
             return np.sum([np.prod(s) for s in self.shape])
         else:
             return np.prod(self.shape)
-    
+
     @property
     def samplepoints(self):
         return self.timecourses.shape[0]
-    
+
     def set_timecourses(self, timecourses):
         self.timecourses = timecourses.reshape(-1, self.num_objects)
-    
+
     def objects_sample(self, samplepoint):
         if type(self.shape) == type([]):
             boundaries = np.cumsum([0] + [np.prod(s) for s in self.shape])
             print boundaries
-            return [self.timecourses[samplepoint, boundaries[ind]:boundaries[ind + 1]].reshape(self.shape[ind]) 
+            return [self.timecourses[samplepoint, boundaries[ind]:boundaries[ind + 1]].reshape(self.shape[ind])
                                     for ind in range(len(self.shape))]
         else:
-            print 'no multiple objects' 
-               
+            print 'no multiple objects'
+
     def shaped2D(self):
         if type(self.shape) == type((1,)):
             return self.timecourses.reshape(-1, *self.shape)
         else:
             print 'multiple object forms'
-    
+
     def trial_shaped(self):
         return self.timecourses.reshape(len(self.label_sample), -1, self.num_objects)
-    
+
     def trial_shaped2D(self):
         return self.timecourses.reshape(len(self.label_sample), -1, *self.shape)
-    
+
     def as_dict(self, aggregate=None):
         if aggregate == 'objects':
             outdict = zip(self.label_objects, self.timecourses.T)
         if aggregate == 'trials':
             outdict = zip(self.label_sample, self.trial_shaped())
         return outdict
-                
+
     def copy(self):
         out = cp.copy(self)
         out.name = cp.copy(self.name)
         out.label_sample = cp.copy(self.label_sample)
-        out.label_objects = cp.copy(self.label_objects)        
+        out.label_objects = cp.copy(self.label_objects)
         if self.typ == 'latent_series':
-            out.base = self.base.copy() 
+            out.base = self.base.copy()
         return out
-    
+
     def save(self, filename):
         data = self.__dict__.copy()
         np.save(filename, data.pop('timecourses'))
@@ -176,7 +176,7 @@ class TimeSeries(object):
             self.base.save(filename + '_base')
             data.pop('base')
         json.dump(data, open(filename + '.json', 'w'))
-            
+
     def load(self, filename):
         self.__dict__.update(json.load(open(filename + '.json')))
         self.timecourses = np.load(filename + '.npy')

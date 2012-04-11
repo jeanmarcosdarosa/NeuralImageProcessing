jeanmarcosdarosa/NeuralImageProcessing

commit 3233a01a67ff6eb8d3780671caef846b0f0eb0ca

use set_timecourses in the TimeSeries init function
To preprocess the data of the micro lesions experiments I had to construct a new TimeSeries object and set its time course in the constructor. I assumed it will automatically reshape the data also when I give them in the trialShaped2D format. I think it should be done in the constructor.

also add some documentation of the TimeSeries Object

NeuralImageProcessing/pipeline.py
@@ -80,21 +80,36 @@ class Block(object):
     def set_receiving(self):
         """reset input buffers after processing"""
         for key in self.input:
-            self.input[key] = '' 
+            self.input[key] = ''
 
 class TimeSeries(object):
-    ''' dim0: timepoints, dim1: objects'''
-    
+    ''' basic data structure to hold data and meta information
+
+        furthermore it can be used to do some basic transformations of the data
+
+        short description of the main variables:
+            * timecourses: numpy array with the actual data
+            * shape: the shape of the original data objects
+                     (e.g. (640, 480) for video data)
+            * typ: description of the data it holds, 2Dimage by default
+            * name: name of the data
+            * data can be divided in samples, each can hold a label
+
+        dim0: timepoints, dim1: objects
+    '''
     def __init__(self, series='', name=['standard_series'], shape=(),
                  typ='2DImage', label_sample=''):
-        self.timecourses = series
         self.shape = shape
+        if isinstance(series, str):
+            self.timecourses = series
+        else:
+            self.set_timecourses(series)
         self.typ = typ
         self.name = name
-        self.label_sample = label_sample 
+        self.label_sample = label_sample
         self.label_objects = []
-        self.shapes = []   
-    
+        self.shapes = []
+
     @property
     def timepoints(self):
         return self.timecourses.shape[0] / len(self.label_sample)

commit d3ea5ea5ee53707b32e157cb068403568ff40dbd

fix the mean bug described in the google doc
the bug was introduced in 3233a01a67ff6eb8d where I started to use set_timecourses in the TimeSeries constructor for correct reshaping. This sometimes added a empty dimension to a TimeSeries object which messed up the filtering of bf.SelectTrials. This behavior is fixed by adding squeeze in set_timeseries

Additionally the first dimension is chose explicitly in the SelectTrials filter. By this the error would have been much easier to spot

NeuralImageProcessing/basic_functions.py
@@ -281,7 +281,7 @@ class SelectTrials():
 
     def __call__(self, timeseries, mask):
         mask = mask.timecourses
-        selected_timecourses = timeseries.trial_shaped()[mask]
+        selected_timecourses = timeseries.trial_shaped()[mask,:,:]
         out = timeseries.copy()
         out.set_timecourses(selected_timecourses)
         out.label_sample = [out.label_sample[i] for i in np.where(mask)[0]]

NeuralImageProcessing/pipeline.py
@@ -130,7 +130,7 @@ class TimeSeries(object):
         return self.timecourses.shape[0]
 
     def set_timecourses(self, timecourses):
-        self.timecourses = timecourses.reshape(-1, self.num_objects)
+        self.timecourses = timecourses.reshape(-1, self.num_objects).squeeze()
 
     def objects_sample(self, samplepoint):
         if type(self.shape) == type([]):
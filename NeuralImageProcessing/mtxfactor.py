85e6f31354b7fa8ceae5c97873c354026927d82e
Stephan Gabler <stephan.gabler@gmail.com>
2012-04-11 12:08:18 +0200

    remove trailing and leading whitespace

--- NeuralImageProcessing/mtxfactor.py
+++ NeuralImageProcessing/mtxfactor.py
@@ -26,27 +26,27 @@ class RRI(object):
 
     def init_factors(self, Y, k, A=None, X=None, Xpart=None, shape=(64, 84)):
         """ generate start matrices U, V """
-        
+
         print 'init started'
         m, n = Y.shape
 
         # sample start matrices
-        if A is None: 
+        if A is None:
             A = np.random.rand(m, k)
 
-        if X is None: 
+        if X is None:
             X = np.ones((k, n))
 
-        # scale A, X with alpha such that || Y - alpha AX ||_fro is 
+        # scale A, X with alpha such that || Y - alpha AX ||_fro is
         # minimized
         AX = np.dot(A, X).flatten()
         # alpha = < Y.flatten(), AX.flatten() > / < AX.flatten(),AX.flatten() >
         print Y.shape, A.shape, X.shape, AX.shape
-        
+
         alpha = np.dot(Y.flatten(), AX) / np.dot(AX, AX)
         A /= np.sqrt(np.abs(alpha) + 1E-10)
         X /= np.sqrt(np.abs(alpha) + 1E-10)
-        
+
         return A, X
 
     def __call__(self, Y, k, A=None, X=None, Xpart=None, eps=1e-5,
@@ -55,9 +55,9 @@ class RRI(object):
         """ basic template for NNMA iterations """
         print 'nnma started'
         self.maxcount = maxcount
-        
+
         YT = Y.T
-        
+
 
         A, X = self.init_factors(Y, k, A, X, Xpart, shape)
         if param['smoothness'] > 0:
@@ -68,7 +68,7 @@ class RRI(object):
                 self.S[pos, pos - 1] = 0
         #self.S[0,1]=2
         #self.S[-1,-2]=2
-        
+
         count = 0
         obj_old = 1e99
 
@@ -88,7 +88,7 @@ class RRI(object):
                 if verbose: print "RESTART"
                 A, X = self.init_factors(Y, k)
                 count = 0
-            
+
             count += 1
 
             # relative distance which is independeant to scaling of A
@@ -99,22 +99,22 @@ class RRI(object):
                 if count % verbose == 0:
                     print "count=%6d obj=%E d_obj=%E" % (count, obj, delta_obj)
 
-            if count >= maxcount: break 
+            if count >= maxcount: break
             # delta_obj should be "almost negative" and small enough:
             if -eps < delta_obj <= 1e-12:
                 break
 
             obj_old = obj
 
-        
+
         if verbose:
             print "FINISHED:"
             print "count=%6d obj=%E d_obj=%E" % (count, obj, delta_obj)
 
         return A, X, obj, count, count < maxcount
-    
+
     def update(self, Y, YT, A, X, count, **param):
-        
+
         psi = param.get("psi", 1e-12)
         sparse_param = param.get("sparse_par", 0)
         sparse_param2 = param.get("sparse_par2", 0)
@@ -122,7 +122,7 @@ class RRI(object):
         smooth_param = param.get("smoothness", 0)
         negbase = param.get("negbase", 0)
         num_mode = A.shape[1]
-        
+
         E = Y - np.dot(A, X)
         indlist = range(A.shape[1])
         #random.shuffle(list)
@@ -133,55 +133,55 @@ class RRI(object):
             aj = A[:, j]
             xj = X[j, :]
 
-            
+
             Rt = E + np.outer(aj, xj)
 
             xj = self.project_residuen(Rt.T, xj, aj, psi, sparse_param, smooth_param, sparse_param2=sparse_param2, X=X)
-            #analysis: 
+            #analysis:
             xj /= np.max(xj) + psi
-            #xj /= np.sqrt(np.sum(xj ** 2)) + psi    
-                                
+            #xj /= np.sqrt(np.sum(xj ** 2)) + psi
+
             rect = False if (j >= (num_mode - negbase)) else True
             aj = self.project_residuen(Rt, aj, xj, psi, 0, rectify=rect, sparse_param2=sparse_param3, X=A.T)
             Rt -= np.outer(aj, xj)
-            
+
             A[:, j] = aj
             X[j, :] = xj
-            
+
             E = Rt
 
         return A, X
-    
+
     def project_residuen(self, res, old, to_base, psi=1e-12, sparse_param=0, smoothness=0, rectify=True, sparse_param2=0, X=0):
-        new_vec = np.dot(res, to_base) + psi * old 
+        new_vec = np.dot(res, to_base) + psi * old
         new_vec -= sparse_param
-        
+
         if sparse_param2 > 0:
             #analysis
             norm = np.sqrt(np.sum(X ** 2, 1)).reshape((-1, 1)) + 1E-15
             occupation = np.sum(X / norm, 0) - old / (np.sqrt(np.sum(old ** 2)) + 1E-15)
-            
+
             #new
-           
+
             #norm = np.mean(X, 1)
             #occupation = np.sum(X - norm.reshape((-1, 1)), 0) - (old - np.mean(old))
             #new end
-            
+
             #if np.sum(occupation[new_vec > 0] > 0):
-                # analysis: new_vec -= sparse_param2 * (occupation - old / (np.sqrt(np.sum(old ** 2)) + 1E-15))       
-                
+                # analysis: new_vec -= sparse_param2 * (occupation - old / (np.sqrt(np.sum(old ** 2)) + 1E-15))
+
                 # new:
-            new_vec -= sparse_param2 * occupation 
-                
+            new_vec -= sparse_param2 * occupation
+
         if smoothness > 0:
             new_vec += smoothness * np.dot(self.S, old * np.max(new_vec))
-        
-        new_vec /= (np.linalg.norm(to_base) ** 2 + psi + smoothness) 
-        
+
+        new_vec /= (np.linalg.norm(to_base) ** 2 + psi + smoothness)
+
         if rectify:
             new_vec[new_vec < 0] = 0
 
-        return new_vec 
+        return new_vec
 
 
 def stJADE(X, **param):
@@ -204,12 +204,12 @@ def stJADE(X, **param):
     % Ss                (matrix) reconstructed spatial source matrix (of dimension n x ms)
     % St                (matrix) reconstructed temporal source matrix (of dimension n x mt)
     %
-    % Here are the valid argument IDs and corresponding values. 
+    % Here are the valid argument IDs and corresponding values.
     %  'lastEig' or     (integer) Number n of components to be estimated. Default equals min(ms,mt). It is strongly
     %  'numOfIC'                 recommended to set a much smaller n here, otherwise cumulant estimates are very bad. Dimension
     %                            reduction is performed using SVD projection along eigenvectors correspoding to largest eigenvalues ('spatiotemporal PCA').
     %  'alpha'          (double) weighting parameter in [0,1] (default 0.5). The larger alpha the more temporal separation is favored.
-    %  'verbose'        (string) report progress of algorithm in text format. 
+    %  'verbose'        (string) report progress of algorithm in text format.
     %                            Either 'on' or 'off'. Default is 'on'.
     %  'orthJD'         (string) restrict JD to orthogonal matrices only - this is much more robust than otherwise,
     %                            however it does not include the full search space (although it usually works well in practice).
@@ -222,25 +222,25 @@ def stJADE(X, **param):
     %
     % ------------------------------------------------------------------------------------------------
     % REFERENCES:
-    %   F.J. Theis, P. Gruber, I. Keck, A. Meyer-Baese and E.W. Lang, 
-    %   'Spatiotemporal blind source separation using double-sided approximate joint diagonalization', 
+    %   F.J. Theis, P. Gruber, I. Keck, A. Meyer-Baese and E.W. Lang,
+    %   'Spatiotemporal blind source separation using double-sided approximate joint diagonalization',
     %   submitted to EUSIPCO 2005 (Antalya), 2005.
     %
     % spatiotemporal ICA (using entropies) has first been introduced by
-    %   J.V. Stone, J. Porrill, N.R. Porter, and I.W. Wilkinson, 'Spatiotemporal independent component 
+    %   J.V. Stone, J. Porrill, N.R. Porter, and I.W. Wilkinson, 'Spatiotemporal independent component
     %   analysis of event-related fmri data using skewed probability density functions',
     %   NeuroImage, 15(2):407-421, 2002.
     %
     % the classical ('temporal') JADE algorithm is described in
     %   J.-F. Cardoso and A. Souloumiac, 'Blind beamforming for non
     %   gaussian signals', IEE Proceedings - F, 140(6):362-370, 1993.
-    %  
-    % for joint diagonalization stJADE uses Cardoso's diagonalization algorithm based on iterative 
+    %
+    % for joint diagonalization stJADE uses Cardoso's diagonalization algorithm based on iterative
     % Given's rotations (matlab-file RJD) in the case of orthogonal searches, see
     %   J.-F. Cardoso and A. Souloumiac, 'Jacobi angles for simultaneous diagonalization',
     %   SIAM J. Mat. Anal. Appl., vol 17(1), pp. 161-164, 1995
     % and Yeredors ACDC in the general case of non-orthogonal search, see
-    %   A. Yeredor, 'Non-Orthogonal Joint Diagonalization in the Least-Squares Sense with Application 
+    %   A. Yeredor, 'Non-Orthogonal Joint Diagonalization in the Least-Squares Sense with Application
     %   in Blind Source Separation, IEEE Trans. On Signal Processing, vol. 50 no. 7 pp. 1545-1553, July 2002.
     '''
 
@@ -251,7 +251,7 @@ def stJADE(X, **param):
     orthJD = param.get('orthjd', True)
     n = param.get('latents', min(ms, mt))
     alpha = param.get('alpha', 0.5)
-    
+
     if verbose:
         print('stJADE: centering data...')
 
@@ -279,7 +279,7 @@ def stJADE(X, **param):
         print 'retained percentage of variance: ', np.sum(Dx[:n]) / np.sum(Dx) * 100
     else:
         print('stJADE: warning - no dimension reduction has been performed. It is strongly recommended to set a much smaller n using parameter ''latents'', otherwise cumulant estimates are very bad.')
-    
+
 
     #########################################################################################
     # Estimation of the multidimensional space delayed covariance matrices
@@ -303,7 +303,7 @@ def stJADE(X, **param):
     #########################################################################################
     # Joint diagonalization
     #########################################################################################
- 
+
     if verbose:
         print('done\nstJADE: performing approximate joint diagonalization of %i (%ix%i)-matrices ', 2 * nummat, n, n);
 
@@ -311,7 +311,7 @@ def stJADE(X, **param):
         if verbose:
             print('using orthogonal JD...')
         W = rjd(np.hstack([M[:, :, i] for i in range(M.shape[2])])).T #M.reshape((n, -1))).T # % orthogonal approximate JD
-    else: #careful: acdc doesn't really like noninvertible matrices       
+    else: #careful: acdc doesn't really like noninvertible matrices
         print 'not yet implemnted'
         #if verbose:
         #    print('using nonorthogonal JD...')
@@ -326,17 +326,17 @@ def stJADE(X, **param):
 
     St = np.dot(np.dot(W , np.diag(np.diag(D) ** -0.5)) , V.T)
     Ss = np.dot(np.dot(U , np.diag(np.diag(D) ** 1.5)), np.linalg.matrix_power(W , -1)).T
-       
+
     # add transformed means
     means = np.mean(X, 0)
     meant = np.mean(X, 1)
-    
+
     pSs = scipy.linalg.pinv(Ss).T
     pSt = scipy.linalg.pinv(St).T
 
     Smeant = np.dot(pSs, meant)
     Smeans = np.dot(pSt, means)
-    
+
     St = St + Smeant.reshape((-1, 1))
     Ss = Ss + Smeans.reshape((-1, 1))
 
@@ -357,7 +357,7 @@ def jadeCummulantMatrices(X, useUnitCov=False):
     if useUnitCov:
         R = np.eye(n)
     else:
-        R = np.cov(X) # covariance    
+        R = np.cov(X) # covariance
 
 
     for p in range(n):
@@ -369,16 +369,16 @@ def jadeCummulantMatrices(X, useUnitCov=False):
         k = k + 1;
         # case q < p
         for q in range(p):
-            
+
             C = np.dot(np.outer(scale , X[p, :] * X[q, :]) * X , X.T) * np.sqrt(2)
             E = np.zeros((n, n))
             E[p, q] = 1 / np.sqrt(2)
             E[q, p] = E[p, q]
             M[:, :, k] = C - np.dot(R, np.dot(E, R)) - np.trace(np.dot(E, R)) * R - np.dot(np.dot(R, E.T), R)
             k = k + 1;
-            
+
     return M
-    
+
 def rjd(A, threshold=np.finfo(float).eps):
     '''
     %***************************************
@@ -406,7 +406,7 @@ def rjd(A, threshold=np.finfo(float).eps):
     % ie a common othonormal set eigenvectors, then the algorithm finds it.
     % The eigenvectors THEN are the column vectors of V
     % and D1, ...,Dn are diagonal matrices.
-    % 
+    %
     % The algorithm implements a properly extended Jacobi algorithm.
     % The algorithm stops when all the Givens rotations in a sweep
     % have sines smaller than 'threshold'.
@@ -416,7 +416,7 @@ def rjd(A, threshold=np.finfo(float).eps):
     % Hence, it is often not necessary to push the accuracy of
     % the rotation matrix V to the machine precision.
     % It is defaulted here to the square root of the machine precision.
-    % 
+    %
     %
     % Author : Jean-Francois Cardoso. cardoso@sig.enst.fr
     % This software is for non commercial use only.
@@ -451,14 +451,13 @@ def rjd(A, threshold=np.finfo(float).eps):
     %
     %
     '''
-    
+
     m, nm = A.shape
     V = np.eye(m)
 
     encore = True
     while encore:
         encore = False
-        print 'next iter'
         for p in range(m):
             for q in range(p, m):
                 ###computation of Givens rotations
@@ -470,7 +469,7 @@ def rjd(A, threshold=np.finfo(float).eps):
                 c = math.cos(theta)
                 s = math.sin(theta)
                 encore = np.logical_or(encore, (abs(s) > threshold))
-                # update of the A and V matrices 
+                # update of the A and V matrices
                 if (abs(s) > threshold):
                     Mp = A[:, p:nm:m]
                     Mq = A[:, q:nm:m]
@@ -483,7 +482,7 @@ def rjd(A, threshold=np.finfo(float).eps):
                     temp = V[:, p]
                     V[:, p] = c * V[:, p] + s * V[:, q]
                     V[:, q] = c * V[:, q] - s * temp
-    qDs = A 
+    qDs = A
     return V #, qDs
 
 '''
@@ -492,12 +491,12 @@ def testjade()
     n=4;
     # make some toy images
     Ss=zeros(64,64,n);
-    for i in range(2*n) 
+    for i in range(2*n)
         a=np.ceil(rand*40)
         b=np.ceil(rand*40)
         da=np.ceil(rand*20)
         db=np.ceil(rand*20);
-    Ss(a:a+da,b:b+db,mod(i,n)+1)=rand(da+1,db+1); 
+    Ss(a:a+da,b:b+db,mod(i,n)+1)=rand(da+1,db+1);
 
 
 % plot them
@@ -534,12 +533,12 @@ pause
 figure, showdata(Stest,'recovered temporal sources using stSOBI')
 figure, showdata(Ssest,'recovered spatial sources using stSOBI')
 
-% give some indication for separation performance 
+% give some indication for separation performance
 % -> the following matrix should be close to a permutation matrix
 Stest*pinv(St)
 
 % stSOBI performance is not excellent because of random structure in images
-% actually autocovs are rather degenerate... (in contrast to real images) 
+% actually autocovs are rather degenerate... (in contrast to real images)
 
 % wait
 pause
@@ -552,7 +551,7 @@ Ssest=mixim(pinv(Stest'),X);
 figure, showdata(Stest,'recovered temporal sources using stJADE')
 figure, showdata(Ssest,'recovered spatial sources using stJADE')
 
-% give some indication for separation performance 
+% give some indication for separation performance
 % -> the following matrix should be close to a permutation matrix
 Stest*pinv(St)
 
